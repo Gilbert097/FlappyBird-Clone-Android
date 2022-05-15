b0d9feb7bd9d3881cefe9f24574682cd1ec7d896
Gilberto Silva <betosilva.luz@gmail.com>
2022-05-15 19:30:03 -0300

    Adicionando imagens de fundo e pássaro.

--- core/src/com/cursoandroid/flappybirdclone/MyFlappyBirdClone.kt
+++ core/src/com/cursoandroid/flappybirdclone/MyFlappyBirdClone.kt
@@ -1,27 +1,33 @@
 package com.cursoandroid.flappybirdclone
 
 import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.utils.ScreenUtils
+import javax.swing.Spring
 
 class MyFlappyBirdClone : ApplicationAdapter() {
     private lateinit var batch: SpriteBatch
-    private lateinit var img: Texture
+    private lateinit var backgroundImg: Texture
+    private lateinit var birdImg: Texture
+
+
     override fun create() {
         batch = SpriteBatch()
-        img = Texture("badlogic.jpg")
+        backgroundImg = Texture("fundo.png")
+        birdImg = Texture("passaro1.png")
     }
 
     override fun render() {
-        ScreenUtils.clear(1f, 0f, 0f, 1f)
+        //ScreenUtils.clear(1f, 0f, 0f, 1f)
         batch.begin()
-        batch.draw(img, 0f, 0f)
+        batch.draw(backgroundImg,  0f,  0f, Gdx.graphics.width.toFloat(), Gdx.graphics.height.toFloat())
+        batch.draw(birdImg,  0f,  0f,)
         batch.end()
     }
 
     override fun dispose() {
         batch.dispose()
-        img.dispose()
+        backgroundImg.dispose()
     }
 }
